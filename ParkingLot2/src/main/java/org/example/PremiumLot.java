c44d0115f40410b5e4a612f6581a0fb427ab064c
Daniel MacKay <84335791+dmackay39@users.noreply.github.com>
2023-10-30 15:24:41 +0000

    Complete premium parking lot

--- ParkingLot2/src/main/java/org/example/PremiumLot.java
+++ ParkingLot2/src/main/java/org/example/PremiumLot.java
@@ -4,6 +4,13 @@ public class PremiumLot implements ParkingLot {
 
     @Override
     public int calculateFee(Ticket ticket) {
+        int hourlyFee = 4;
+
+        int days = ticket.getDays();
+        int hours = ticket.getHours();
+        int minutes = ticket.getMinutes();
+
+        int bill = 0;
 // ToDo:  Use TDD to Implement this function
 //  You will need to mock the Ticket interface
 //  Business rules for this parking lot are as follows:
@@ -13,6 +20,21 @@ public class PremiumLot implements ParkingLot {
 //        Daily max is $27
 //        Every 7th day or fraction thereof is free.  (ie, a stay of 13 days and 4 hours will be billed for 12 days
 
-        return 0;
+        if (days > 0){
+            bill += 27*(days - days/7);
+        }
+        if (minutes > 0){
+            hours++;
+        }
+        if ((days+1)%7 ==0){
+            hours = 0;
+        }
+        if (hours > 6){
+            bill += 27;
+        } else {
+            bill += hourlyFee * hours;
+        }
+
+        return bill;
     }
 }
--- ParkingLot2/src/test/java/org/example/PremiumLotTests.java
+++ ParkingLot2/src/test/java/org/example/PremiumLotTests.java
@@ -1,4 +1,72 @@
 package org.example;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class PremiumLotTests {
+    PremiumLot uut;
+    Ticket ticket;
+
+    @BeforeEach
+    public void setUp(){
+        uut = new PremiumLot();
+        ticket = mock(Ticket.class);
+    }
+
+    @AfterEach
+    public void tearDown(){
+        uut = null;
+        ticket = null;
+    }
+
+    @ParameterizedTest
+    @CsvSource({"0,0", "1,4"})
+    public void testHourlyRate(int hours, int result){
+        when(ticket.getHours()).thenReturn(hours);
+        int actualResult = uut.calculateFee(ticket);
+        Assertions.assertEquals(result, actualResult);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"7,27", "6, 24"})
+    public void testDailyMax(int hours, int result){
+        when(ticket.getHours()).thenReturn(hours);
+        int actualResult = uut.calculateFee(ticket);
+        Assertions.assertEquals(result, actualResult);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"0,7,27", "0, 6, 24", "1, 6, 51", "1, 7, 54"})
+    public void testMultipleDays(int days, int hours, int result){
+        when(ticket.getHours()).thenReturn(hours);
+        when(ticket.getDays()).thenReturn(days);
+        int actualResult = uut.calculateFee(ticket);
+        Assertions.assertEquals(result, actualResult);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"0,7,4", "6, 6, 27"})
+    public void testMinutesRoundUp(int hours, int minutes, int result){
+        when(ticket.getHours()).thenReturn(hours);
+        when(ticket.getMinutes()).thenReturn(minutes);
+        int actualResult = uut.calculateFee(ticket);
+        Assertions.assertEquals(result, actualResult);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"6,4,162", "5, 6, 159","13,4,324"})
+    public void testSeventhDay(int days, int hours, int result){
+        when(ticket.getDays()).thenReturn(days);
+        when(ticket.getHours()).thenReturn(hours);
+        int actualResult = uut.calculateFee(ticket);
+        Assertions.assertEquals(result, actualResult);
+    }
+
 }
